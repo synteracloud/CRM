8c5ab98433f9da2391c202090f6fdba13bf1f1ea
synteracloud <synteracloud@gmail.com>
2026-03-26 18:16:29 +0500

    Add opportunity-based forecasting model and aggregation APIs

--- gateway/routes/index.js
+++ gateway/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const usersV1Router = require('./v1-users.routes');
+const forecastsV1Router = require('./v1-forecasts.routes');
 const { respondError } = require('../middleware/response-wrapper');
 
 const router = express.Router();
 
 router.use('/api/v1/users', usersV1Router);
+router.use('/api/v1/forecasts', forecastsV1Router);
 
 router.use((req, res) => respondError(res, 'not_found', 'The requested resource was not found.', [], 404));
 
