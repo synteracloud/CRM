dba98c39fc29aecf7161ce9ab3c241d87da33397
synteracloud <synteracloud@gmail.com>
2026-04-02 02:08:16 +0500

    Add Pakistan payment adapter layer with webhook and retry handling

--- adapters/interfaces/__init__.py
+++ adapters/interfaces/__init__.py
@@ -1,4 +1,5 @@
 from .messaging_adapter import MessagingAdapter
+from .payment_adapter import PaymentAdapter
 from .types import AdapterContext, AdapterError, AdapterErrorCode
 
-__all__ = ["MessagingAdapter", "AdapterContext", "AdapterError", "AdapterErrorCode"]
+__all__ = ["MessagingAdapter", "PaymentAdapter", "AdapterContext", "AdapterError", "AdapterErrorCode"]
